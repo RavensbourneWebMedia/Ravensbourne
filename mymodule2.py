af102ec67f62b677f5155e935fefba82e16869f4
mimi-engineer <maryam.ahmed@eng.ox.ac.uk>
2016-11-28 01:15:43 +0000

    Add files via upload

---
+++ mymodule2.py
@@ -0,0 +1,18 @@
+############################################ mymodule2: practising functions ########################################
+###################################### Maryam Ahmed, University of Oxford, 2016 ####################################
+
+# INSTRUCTIONS
+# Write code for the three functions below:
+
+# This function should use 'raw_input' to ask for the user's age in YEARS, and return the user's age in MONTHS
+def convertAge():
+    return # This function isn't returning anything right now; this is for you to fill in!
+
+# This function should take in a distance in MILES, and return the distance in KILOMETRES
+def miles2Km(miles):
+    return # This function isn't returning anything right now; this is for you to fill in!
+
+# This function should take in a WORD and return its LENGTH
+def wordLength(word):
+    return # This function isn't returning anything right now; this is for you to fill in!
+
